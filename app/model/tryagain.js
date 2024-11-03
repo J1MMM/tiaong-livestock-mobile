48977f68c090eced92e8f8c08099322cea113993
J1MMM <jimuelbarero00@gmail.com>
2024-11-03 19:50:55 +0800

    second commit

--- app/model/tryagain.js
+++
@@ -1,7 +0,0 @@
-const sound1 = require('../../assets/audio/tryagain1.mp3')
-const sound2 = require('../../assets/audio/tryagain2.mp3')
-const sound3 = require('../../assets/audio/tryagain3.mp3')
-const sound4 = require('../../assets/audio/tryagain4.mp3')
-const sound5 = require('../../assets/audio/tryagain5.mp3')
-
-export default tryAgain = [sound1, sound2, sound3, sound4, sound5]
