ca0a7ceedbb62f536433d71d96fef692c2e7fc1f
J1MMM <jimuelbaraero00@gmail.com>
2025-01-16 11:51:22 +0800

    123123123

--- app/screens/PersonalInfoFrom1.js
+++ app/screens/PersonalInfoFrom1.js
@@ -47,12 +47,15 @@ const PersonalInfoFrom1 = () => {
 
     // Validate surname, firstname, and sex for letters only
     if (
-      !lettersOnlyRegex.test(userData.middlename) ||
+      (!lettersOnlyRegex.test(userData.extensionName) &&
+        userData.extensionName) != "" ||
+      (!lettersOnlyRegex.test(userData.middlename) &&
+        userData.middlename != "") ||
       !lettersOnlyRegex.test(userData.surname) ||
       !lettersOnlyRegex.test(userData.firstname)
     ) {
       setErrMsg(
-        "Surname, Firstname and Middlename  should contain letters only."
+        "Surname, Firstname, Middlename and Extension name should contain letters only."
       );
       return;
     }
